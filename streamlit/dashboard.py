b2f957bd4716162c4a4c87368bbed144d88c57d7
Kristian André Jakobsen <k.andrejakobsen@gmail.com>
2023-08-07 23:10:03 +0200

    tweets pretty data filtered by date

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -1,4 +1,6 @@
+import datetime
 import random
+import time
 
 import pandas as pd
 import plotly.express as px
@@ -6,7 +8,7 @@ import psycopg2
 from psycopg2 import OperationalError
 from psycopg2.extensions import connection
 from pyecharts import options as opts
-from pyecharts.charts import EffectScatter, Gauge, HeatMap, Line
+from pyecharts.charts import EffectScatter, Gauge, HeatMap, Line, Scatter
 from pyecharts.commons.utils import JsCode
 from pyecharts.faker import Faker
 from pyecharts.globals import SymbolType
@@ -15,13 +17,21 @@ from streamlit_echarts import st_echarts, st_pyecharts
 import streamlit as st
 
 WATER_COLOR = "#566D74"
-
+MIN_DATE = datetime.date(year=2030, month=4, day=1)
+N_DAYS = 2549
+DATE_RANGE = pd.DataFrame(
+    {
+        "date": pd.date_range(start=MIN_DATE, periods=N_DAYS).date,
+        "day": range(1, N_DAYS + 1),
+    }
+)
 
 st.set_page_config(layout="wide")
 
 
 def main(con: connection) -> None:
-    st.header("Save Zakar Hackathon!")
+    global geo
+    geo = [15, 15]
     temp = fetch_sql(
         con,
         sql="""
@@ -29,53 +39,100 @@ def main(con: connection) -> None:
                 *
             FROM raw.temperature_readings;""",
     )
-    tweets = fetch_sql(
-        con,
-        """
-        WITH top_tweets AS (
-            SELECT
-                t.geospatial_x,
-                t.geospatial_y,
-                t.tweet,
-                ROW_NUMBER() OVER(PARTITION BY
-                    t.geospatial_x,
-                    t.geospatial_y
-                ORDER BY t.day DESC) AS rank
-        FROM raw.tweets t)
-    SELECT * FROM top_tweets WHERE rank <= 3""",
-    )
-    st.dataframe(tweets)
-    l, left, right, r = st.columns([1, 35, 35, 1], gap="small")
+    _, mid, _ = st.columns([1, 5, 1])
+    with mid:
+        st.header("Welcome to Zakar!")
+        st.write(
+            """We are in the year 2037 at an idyllic island full of biodiversity and friendly
+            people. But despite all the beauty, the small island of
+            Zakar is plagued by regular fires. Luckily, by the ingenuity of Zakar's people,
+            they have been able to develop an early fire detection system.
+            Use this dashboard to understand the trends and patterns of these fires,
+            and maybe you will be able to help us improve our alerting system!"""
+        )
+    l, left, right, r = st.columns([1, 35, 25, 1], gap="small")
     h = plot_heatmap(temp)
     with left:
+        st.subheader("Please choose a location on the map")
         values = st_pyecharts(
             h,
             events={"click": "function(params) {return params.value}"},
-            width=700,
-            height=500,
+            width=780,
+            height=585,
         )
-        geo = values[:2] if values else [0, 0]
-        st.text(f"Currently selected coordinates: {geo[0], geo[1]}")
+        geo = values[:2] if values else geo
         df_filtered = temp.query(f"(geospatial_x=={geo[0]}) & (geospatial_y=={geo[1]})")
 
+    with st.sidebar:
+        fire_icon(100)
+        date = st.date_input(
+            label="Choose a date",
+            value=MIN_DATE,
+            min_value=MIN_DATE,
+            max_value=DATE_RANGE["date"].max(),
+        )
+        st.caption("By Kristian André Jakobsen & Alessandra Oshiro")
+    tweets = fetch_tweets(
+        con,
+        int(DATE_RANGE.loc[DATE_RANGE["date"] == date, "day"].iloc[0]),
+        geo[0],
+        geo[1],
+    )
     with right:
-        inner_left, inner_right = st.columns([0.1, 0.9])
-        with inner_left:
-            tweet_icon()
-        with inner_right:
-            st.subheader("Tweets")
-        st.write("---")
-        st.write(tweets.iloc[0, 2])
-        st.write("---")
-        st.write(tweets.iloc[1, 2])
-        st.write("---")
-        st.write(tweets.iloc[2, 2])
-        st.write("---")
+        st.subheader(f"Tweets for {date:%d} {date:%B} {date:%Y} at {geo[0], geo[1]}")
+        if tweets.empty:
+            st.info("There are no tweets for the selected date and location.")
+        for t in tweets["tweet"]:
+            st.text("")
+            tweet(t)
     st.empty()
     st_pyecharts(
         plot_line(df_filtered, geo),
         # height=300,
         # width=700,
+    )
+
+
+def fetch_tweets(con, day, geo_x, geo_y):
+    return fetch_sql(
+        con,
+        sql="""
+        WITH top_tweets AS (
+            SELECT
+                t.day,
+                t.geospatial_x,
+                t.geospatial_y,
+                t.tweet,
+                ROW_NUMBER() OVER(PARTITION BY
+                    t.day,
+                    t.geospatial_x,
+                    t.geospatial_y
+                ORDER BY t.day, t.geospatial_x, t.geospatial_y DESC) AS rank
+                FROM raw.tweets t)
+        SELECT * FROM top_tweets tt
+        WHERE rank <= 4
+        AND tt.day = %(day)s
+        AND tt.geospatial_x = %(geo_x)s
+        AND tt.geospatial_y = %(geo_y)s;
+        """,
+        parameters={
+            "day": day,
+            "geo_x": geo_x,
+            "geo_y": geo_y,
+        },
+    )
+
+
+def tweet(text="test", width=650):
+    # inner_left, inner_right, _ = st.columns([0.07, 0.85, 0.09])
+    st.markdown(
+        f"""
+        <div style="width:{width}px;">
+        <img src="https://i.imgur.com/ZsKRyWw.png" alt="tweet" style="width:100%;">
+        <div style="display:inline-block;position:absolute;top:12px;left:45px;width:{width-55}px;">{text}</div>
+        </div>
+        """,
+        unsafe_allow_html=True,
     )
 
 
@@ -88,13 +145,13 @@ def tweet_icon(width=50):
 
 def fire_icon(width=50):
     st.image(
-        "https://media.tenor.com/8McIGu0Tf_QAAAAi/fire-joypixels.gif",
+        "https://i.imgur.com/Q06cnvr.gif",
         width=width,
     )
 
 
 def plot_heatmap(df):
-    return (
+    h = (
         HeatMap()
         .add_xaxis(range(30))
         .add_yaxis(
@@ -107,7 +164,10 @@ def plot_heatmap(df):
             ),
         )
         .set_global_opts(
-            title_opts=opts.TitleOpts(is_show=False),
+            title_opts=opts.TitleOpts(
+                # title="Choose a location on the map", pos_top=-10, pos_right="middle",
+                is_show=False
+            ),
             legend_opts=opts.LegendOpts(is_show=False),
             tooltip_opts=opts.TooltipOpts(is_show=False),
             xaxis_opts=opts.AxisOpts(
@@ -153,13 +213,25 @@ def plot_heatmap(df):
                     ),
                     graphic_imagestyle_opts=opts.GraphicImageStyleOpts(
                         image="https://i.imgur.com/Qq3UJE9.png",
-                        width=700,
-                        height=500,
+                        width=800,
+                        height=600,
                         # opacity=0.4,
                     ),
                 )
             ],
         )
+    )
+    s = (
+        EffectScatter()
+        .add_xaxis([random.randint(0, 29) for _ in range(5)])
+        .add_yaxis(
+            "",
+            [random.randint(0, 29) for _ in range(5)],
+            symbol="image://https://media.tenor.com/8McIGu0Tf_QAAAAi/fire-joypixels.gif",
+            symbol_size=20,
+            effect_opts=opts.EffectOpts(period=2, scale=2, trail_length=0.9),
+        )
+        .set_global_opts(title_opts=opts.TitleOpts(is_show=False))
     )
 
     return h
@@ -170,15 +242,13 @@ def plot_heatmap(df):
 
 
 def plot_line(df, geo) -> Line:
-    start_date = pd.to_datetime("01-04-2030")
+    st.subheader(f"Historical Temperature at {geo[0], geo[1]}")
     return (
         Line(
             init_opts=opts.InitOpts(
-                animation_opts=opts.AnimationOpts(
-                    animation_duration_update=500, animation_duration=2000
-                )
+                animation_opts=opts.AnimationOpts(animation_duration=3000)
             )
-        ).add_xaxis(pd.date_range(start=start_date, periods=df["day"].max()))
+        ).add_xaxis(df["date"])
         # .add_xaxis([1, 2, 3])
         .add_yaxis(
             series_name="°F",
@@ -200,7 +270,7 @@ def plot_line(df, geo) -> Line:
         #     areastyle_opts=opts.AreaStyleOpts(color="#FF5733", opacity=0.5),
         # )
         .set_global_opts(
-            title_opts=opts.TitleOpts(title=f"Temperature"),
+            title_opts=opts.TitleOpts(is_show=False),
             tooltip_opts=opts.TooltipOpts(
                 trigger="axis",
             ),
@@ -256,16 +326,15 @@ def fetch_sql(
 ) -> pd.DataFrame:
     # try:
     with _connection:
-        return pd.read_sql(sql, _connection)
+        df = pd.read_sql(sql, _connection, params=parameters)
+        df = df.merge(DATE_RANGE, how="left").sort_values("day")
+        return df
     # finally:
     #     _connection.commit()
 
 
 # con = init_connection()
 if __name__ == "__main__":
-    with st.sidebar:
-        fire_icon(100)
-        st.caption("By Kristian André Jakobsen & Alessandra Oshiro")
     con = init_connection()
     main(con)
     # con.close()
